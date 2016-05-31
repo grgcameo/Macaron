04377b2f64b87db90830fe1c591d24c21bcd175b
daeseong.seo <grgcameo@gmai.com>
2016-06-01 00:20:03 +0900

    Add pygame example.

--- Pineapple/ChoPy/bonjour.py
+++ Pineapple/ChoPy/bonjour.py
@@ -1,3 +1,30 @@
-print ('bonjour super peak python')
-print (1+1)
+# coding=utf-8
+import threading
+import random
+
+class Messenger(threading.Thread):
+    def run(self):
+        for _ in range(10):
+            print (threading.currentThread().getName())
+
+
+a = Messenger(name = "a 입니다.")
+b = Messenger(name = "b 입니다.")
+
+
+
+a.start()
+b.start()
+
+
+
+class ChatOn(threading.Thread):
+    def run(self):
+        print (random.randrange(1, 5))
+
+
+c = ChatOn()
+
+c.start()
+
 

bb0ea57d6a5f6c79f451f85d1f9ec1a77b2b4b24
daeseong.seo <grgcameo@gmail.com>
2016-06-23 21:43:09 +0900

    추가

--- Pineapple/HtmlFileToCode/HtmlFileToCode.py
+++ Pineapple/HtmlFileToCode/HtmlFileToCode.py
@@ -5,9 +5,9 @@ import time
 class HtmlFileToCode:
 
     # Discriptor
-    def _DoMakeHtmlCodeDescriptor(self, strOutFileName):
-        outFile = open(strOutFileName, 'a')
-        strFIleName = '//  Filename: {} \n'.format(strOutFileName)
+    def _DoMakeDescriptor(self, strOutFile):
+        outFile = open(strOutFile, 'a')
+        strFIleName = '//  Filename: {} \n'.format(strOutFile)
         strCreateDate = '//  Creation Date: {} \n'.format(time.strftime('%Y.%m.%d'))
 
         discriptorList = [
@@ -23,11 +23,10 @@ class HtmlFileToCode:
         outFile.writelines(discriptorList)
         outFile.close()
 
-
     # 클래스 정의
-    def _DoMakeHtmlCodeClassInfo(self, strOutFileName, strClassName):
+    def _DoMakeClass(self, strOutFile, strClassName):
 
-        outFile = open(strOutFileName, 'a')
+        outFile = open(strOutFile, 'a')
         strClassName = 'class {} \n'.format(strClassName)
 
         classInfoList = [
@@ -46,12 +45,55 @@ class HtmlFileToCode:
         outFile.writelines(classInfoList)
         outFile.close()
 
+    # 본문 작성
+    def _DoMakeContents(self, strOutFile, strInputFile, strClassName):
+
+        inputFile = open(strInputFile, 'r')
+        outFile = open(strOutFile, 'a')
+        strContents = inputFile.read()
+
+        contentsVariablePartList = [
+            'namespace HSP {\n',
+            '   const char *  ',strClassName,'::s_strURL         = \"/{}\";\n'.format(strInputFile),
+            '   unsigned int  ',strClassName,'::s_uContentLength = {};\n'.format(len(strContents)),
+            '   unsigned char ',strClassName,'::s_pContentBody[] = {\n',
+            ]
+
+        contentsArrayPartList = ['        ']
+
+        nCount = 0
+
+        for ch in strContents:
+            contentsArrayPartList.append(str(ord(ch)) + ',')
+            nCount += 1
+            if (nCount == 16):
+                nCount = 0
+                contentsArrayPartList.append('\n        ')
+
+        contentsTailPartList = [
+            '\n'
+            '};\n'
+            '\n',
+            '} // end if namespace\n',
+        ]
+
+        outFile.writelines(contentsVariablePartList)
+        outFile.writelines(contentsArrayPartList)
+        outFile.writelines(contentsTailPartList)
 
 
+        inputFile.close()
+        outFile.close()
+
+
+
+# main test
 test = HtmlFileToCode()
+iName = 'myClass.html'
 fName = 'myClass.cpp'
 cName = 'myClass'
-test._DoMakeHtmlCodeDescriptor(fName)
-test._DoMakeHtmlCodeClassInfo(fName, cName)
+test._DoMakeDescriptor(fName)
+test._DoMakeClass(fName, cName)
+test._DoMakeContents(fName, iName, cName)
 
 

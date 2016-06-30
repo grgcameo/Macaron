f3725a6f4039f669c9f75bdacc0c7d13c7dcb9ed
daeseong.seo <grgcameo@gmail.com>
2016-06-30 17:44:21 +0900

    Add event handle sample

--- Pineapple/HtmlFileToCode/convertWIN.py
+++ Pineapple/HtmlFileToCode/convertWIN.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\Workspace\SourceTree\Macaron\Pineapple\HtmlFileToCode\resource\convertWIN.ui'
+# Form implementation generated from reading ui file 'D:\Workspace\SourceTree\Macaron\Pineapple\HtmlFileToCode\convertWIN.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
--- Pineapple/HtmlFileToCode/main.py
+++ Pineapple/HtmlFileToCode/main.py
@@ -1,10 +1,40 @@
 # coding = utf-8
-import htmltocode
-import resource.converWIN
+
 import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
 
-# main
-app = convertWIN()
+form_class = uic.loadUiType("convertWIN.ui")[0]
 
-#convertor = HtmlFileToCode.HtmlFileToCode()
-#convertor.SelectFileConvert('myClass.html')
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setupUi(self)
+
+        #event connect
+        self.connect(self.checkBox_SelectFile, SIGNAL("clicked()"), self.checkBox_SelectFile_Clicked)
+        self.connect(self.btn_Convert, SIGNAL("clicked()"), self.btn_Convert_Clicked)
+        self.connect(self.btn_ShutDown, SIGNAL("clicked()"), self.btn_ShutDown_Clicked)
+
+
+    #event handle
+    def checkBox_SelectFile_Clicked(self):
+        QMessageBox.about(self, "message", "checkBox_SelectFile")
+
+    def btn_Convert_Clicked(self):
+        QMessageBox.about(self, "message", "convert")
+
+    def btn_ShutDown_Clicked(self):
+        QMessageBox.about(self, "message", "ShutDown")
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
+
